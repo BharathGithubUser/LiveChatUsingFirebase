c788b51224f7349b837d0abe854f7f3cddc276e9
BharathGithubUser <sbharathrao1995@gmail.com>
2018-07-05 13:24:53 +0530

    successfully updated the code for Splash and Login Screen

---
+++ app/src/main/java/com/pulse/firebasechat/BackgroundImage.java
@@ -0,0 +1,15 @@
+package com.pulse.firebasechat;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+public class BackgroundImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_background_image);
+    }
+}
